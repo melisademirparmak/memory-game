3d9e6491ec5cdbda22b51042c4ae76d56deac0c1
melisademirparmak <melisademirparmaak@gmail.com>
2021-07-28 23:41:30 +0300

    Pokemon api

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
-import './App.css';
+import './index.css';
+
+const url = 'https://pokeres.bastionbot.org/images/pokemon';
 
 function App() {
+  const poke = [
+    { id: 1, name: 'balbasaur' },
+    { id: 8, name: 'wartotle' },
+    { id: 9, name: 'blastoise' },
+    { id: 6, name: 'charizard' },
+    { id: 17, name: 'pidgeot' },
+    { id: 3, name: 'charmander' },
+    { id: 15, name: 'rattata' },
+    { id: 11, name: 'pidgey' },
+  ];
   return <div className="app"></div>;
 }
 
